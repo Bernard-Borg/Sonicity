880a833a99dfc0dc49cde43ac71b4870eb74c0a6
Bernard Borg <bernard.borg36@gmail.com>
2024-02-02 17:49:31 +0100

    Fixed packages, added icon

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     clearScreen: false,
     // tauri expects a fixed port, fail if that port is not available
     server: {
-        port: 1420,
+        port: 49156,
         strictPort: true
     },
     // to make use of `TAURI_DEBUG` and other env variables
